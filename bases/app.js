122c3c0b6ed676173ba32fef01ede45c42c9d390
Alejandro E. Rendon <alejandro@rendon.co>
2025-08-03 12:53:49 -0500

    Refactor app.js and app.ts to use const and let instead of var; add basic functions and default parameters; update tsconfig.json to target ES6 for improved compatibility.

--- bases/app.js
+++ bases/app.js
@@ -1,26 +1,26 @@
 "use strict";
-(function () {
+(() => {
     // Tipos
-    var batman = 'Bruce';
-    var superman = 'Clark';
-    var existe = false;
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
     // Tuplas
-    var parejaHeroes = [batman, superman];
-    var villano = ['Lex Lutor', 5, true];
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
     // Arreglos
-    var aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
     //Enumeraciones
-    var fuerza;
+    let fuerza;
     (function (fuerza) {
         fuerza[fuerza["acuaman"] = 0] = "acuaman";
         fuerza[fuerza["batman"] = 1] = "batman";
         fuerza[fuerza["flash"] = 5] = "flash";
         fuerza[fuerza["superman"] = 100] = "superman";
     })(fuerza || (fuerza = {}));
-    var fuerzaFlash = fuerza.flash;
-    var fuerzaSuperman = fuerza.superman;
-    var fuerzaBatman = fuerza.batman;
-    var fuerzaAcuaman = fuerza.acuaman;
+    const fuerzaFlash = fuerza.flash;
+    const fuerzaSuperman = fuerza.superman;
+    const fuerzaBatman = fuerza.batman;
+    const fuerzaAcuaman = fuerza.acuaman;
     // Retorno de funciones
     function activar_batiseñal() {
         return 'activada';
@@ -29,7 +29,32 @@
         console.log('Auxilio!!!');
     }
     // Aserciones de Tipo
-    var poder = '100';
-    var largoDelPoder = poder.length;
+    const poder = '100';
+    const largoDelPoder = poder.length;
     console.log(largoDelPoder);
+    // Funciones Básicas
+    function sumar(a, b) {
+        return a + b;
+    }
+    const contar = (heroes) => {
+        return heroes.length;
+    };
+    const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+    contar(superHeroes);
+    //Parametros por defecto
+    const llamarBatman = (llamar = true) => {
+        if (llamar) {
+            console.log("Batiseñal activada");
+        }
+    };
+    llamarBatman();
+    // Rest?
+    const unirheroes = (...personas) => {
+        return personas.join(", ");
+    };
+    // Tipo funcion
+    const noHaceNada = (numero, texto, booleano, arreglo) => { };
+    // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+    let noHaceNadaTampoco;
+    noHaceNadaTampoco = noHaceNada;
 })();
